28376b48443b8890eb3d5dc5dc69add69de84b68
allisonlynnbasore14 <allison.basore@students.olin.edu>
2017-05-01 21:11:34 -0400

    Update MusicWebApp.py

--- MusicWebApp.py
+++ MusicWebApp.py
@@ -2,7 +2,6 @@
 This is the page setting up the webapp for Olin College Software Design Project: Music Generation 
 The there are two HTML tempates assocaited with this web app:
 Toolbox.HTML and UserInterface.HTML.
-
 In order to run, you must download and open "SONIC PI" 
 """
 
@@ -13,35 +12,40 @@ app = Flask(__name__)
 @app.route('/', methods = ['GET', 'POST'])
 def profile():
 	if request.method == 'POST':
-		song = request.form['song'] #gets the requested song from the first web page
+		song = request.form['song']
+		bassline = request.form['bassline']
+		 #gets the requested song from the first web page
 		#The Best Song Ever Midi doe not currently work
 
 		#Below are the song options and their associated pictures
 		#the pic is a picture related to the song
 		#the call to the main function calls the MusicGeneration.py to make the song
+
 		if song == "The Best Song Ever":
-			main('UpAllNight.mid')
+			main('WhatMakesYouBeautiful.mid',bassline)
 			pic = "https://i.kinja-img.com/gawker-media/image/upload/s--qn6H3zL3--/c_scale,fl_progressive,q_80,w_800/jiszvtpozcrbbzxrxmq6.jpg"
 		elif song == "Twinkle Twinkle Little Star":
-			main('TwinkleTwinkleLittleStar.mid')
+			main('TwinkleTwinkleLittleStar.mid',bassline)
 			pic = "http://img.clipartall.com/yellow-star-border-clip-art-clipart-panda-free-clipart-images-yellow-star-clipart-552_599.png"
 		elif song == "What Makes You Beautiful":
-			main('WhatMakesYouBeautiful.mid')
+			main('WhatMakesYouBeautiful.mid',bassline)
 			pic = "http://www.billboard.com/files/stylus/1977869-one-direction-brisbane-617-409.jpg"
 		elif song == "Up All Night":
-			main('UpAllNight.mid')
+			main('UpAllNight.mid',bassline)
 			pic = "https://cdn.gobankingrates.com/wp-content/uploads/One-Direction.png"
 		else:
+			main('WhatMakesYouBeautiful.mid',bassline)
 			pic = "https://scstylecaster.files.wordpress.com/2016/01/one-direction-breaking-up.jpg"
 
 		#This sets the template up with the recieved variables of song and picture	
-		return render_template("UserInterface.html", song = song, pic = pic)
+
+		return render_template("UserInterface.html", song = song)
 	
 	#Renders the second pape template
-	return render_template("Toolbox.html")
+	return render_template("index.html")
 
 
 
 
 if __name__ == "__main__":
-	app.run().run()
+app.run()#.run()
